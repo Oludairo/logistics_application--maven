598efd915280f93bc1c8582b949b1b7f0f5435b3
Oludairo <eoludairo61@gmail.com>
2021-11-09 18:23:32 +0100

    package trackng controller for tracking package info

--- src/main/java/african/semicolon/cheatah/controllers/PackageTrackingController.java
+++ src/main/java/african/semicolon/cheatah/controllers/PackageTrackingController.java
@@ -1,2 +1,22 @@
-package african.semicolon.cheatah.controllers;public class PackageTrackingController {
+package african.semicolon.cheatah.controllers;
+
+import african.semicolon.cheatah.data.models.TrackingInformation;
+import african.semicolon.cheatah.dtos.requests.AddTrackingInfoRequest;
+import african.semicolon.cheatah.services.TrackingService;
+import african.semicolon.cheatah.services.TrackingServiceImpl;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class PackageTrackingController {
+    private final TrackingService trackingService = new TrackingServiceImpl();
+
+    @PatchMapping("/api/trackingInfo")
+    public Object addTrackingInfo(@RequestBody AddTrackingInfoRequest addTrackingInfoRequest) {
+        return trackingService.updateTrackingInfo(addTrackingInfoRequest);
+    }
+
+    @GetMapping("/api/trackingInfo/{id}")
+    public TrackingInformation getTrackingInfo(@PathVariable("id") Integer trackingId) {
+        return trackingService.trackPackage(trackingId);
+    }
 }
